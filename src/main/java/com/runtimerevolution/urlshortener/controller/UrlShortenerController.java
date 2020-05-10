639b6446ec6a1a3ce70cafecc751171cd240e66f
Rafael Coutinho <rafacoutinho@gmail.com>
2020-05-10 02:05:09 +0100

    Removing favicon

--- src/main/java/com/runtimerevolution/urlshortener/controller/UrlShortenerController.java
+++ src/main/java/com/runtimerevolution/urlshortener/controller/UrlShortenerController.java
@@ -9,10 +9,7 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.ModelAndView;
 
 import javax.servlet.http.HttpServletRequest;
@@ -70,6 +67,11 @@ public class UrlShortenerController {
         model.addAttribute("shortenedUrl", shortenedUrl);
         model.addAttribute("errorMessage", errorMessage);
         return "home";
+    }
+
+    @GetMapping("favicon.ico")
+    @ResponseBody
+    void returnNoFavicon() {
     }
 
     @GetMapping("/{shortKey}")
